1aa00e0203d884e902e43ac360418821d40c5a9a
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-12 18:55:48 -0500

    Componentes personalizados

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import { Navbar} from '../components/Navbar'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 
@@ -13,7 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <main className={styles.main}>
+        <h1>Home Page</h1>
           <h1 className={styles.title}>
             {/* Ir a <a href="/about">About</a> */}
             Ir a <Link href={'/about'}>About</Link>
