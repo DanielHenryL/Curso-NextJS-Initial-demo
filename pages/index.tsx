36cd78eb3ec1c7f7b6c3010e65edcce3b2e2a2c7
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-12 23:35:08 -0500

    Layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,17 @@
-import Head from 'next/head'
-import { Navbar} from '../components/Navbar'
-import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-
+import {MainLayout} from '../components/Layouts/MainLayout'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home - Daniel lagunas </title>
-        <meta name="description" content="About Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <main className={styles.main}>
-        <h1>Home Page</h1>
-          <h1 className={styles.title}>
-            {/* Ir a <a href="/about">About</a> */}
+    <MainLayout title={'daniel lagunas'} content={'Home Page'}>
+      <h1>Home Page</h1>
+          <h1 className={'description'}>
             Ir a <Link href={'/about'}>About</Link>
           </h1>
-          <p className={styles.main}>
+          <p className={'main'}>
             Get started by editing{' '};
-            <code className={styles.code}>pages/home.tsx</code>
-          </p>
-      </main>
-    </>
+            <code>pages/home.tsx</code>
+        </p>
+    </MainLayout>
   )
 }
