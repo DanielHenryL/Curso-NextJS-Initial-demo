b10b40afce63eb7b2fdfd977236ab9c1bd7531cd
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-13 19:54:53 -0500

    Tarea y fin de la seccion

--- pages/pricing/index.jsx
+++ pages/pricing/index.jsx
@@ -1,17 +1,17 @@
 import Link from 'next/link'
 import { MainLayout } from '@/components/Layouts/MainLayout'
 
-export default function ContactPage()  {
+export default function PricingPage()  {
   return (
-    <MainLayout title={'Daniel Contact'} content={'Contact Page'}>
-      <h1>Contact Page</h1>
+    <MainLayout title={'Daniel Pricing'} content={'Pricing Page'}>
+      <h1>Pricing Page</h1>
 
       <h1 className={'description'}>
         Ir a <Link href={'/'}>Home</Link>
       </h1>
       <p className={'main'}>
         Get started by editing{' '};
-        <code >pages/contact/index.jsx</code>
+        <code >pages/pricing/index.jsx</code>
       </p>
     </MainLayout>
   )
